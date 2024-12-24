1f326586acb967c45ca563f73fb71d69e826ade9
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2024-12-24 08:54:06 +0300

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,19 @@
-use hydration::{AutoReload, HydrationScripts};
 use leptos::prelude::*;
-use leptos::{config::LeptosOptions, *};
 use leptos_router::components::{Outlet, ParentRoute, Route, Router, Routes};
 use leptos_router::path;
 
-pub fn shell(options: LeptosOptions) -> impl IntoView {
-    view! {
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <AutoReload options=options.clone() />
-                <HydrationScripts options />
-            </head>
-            <body>
-                <App />
-            </body>
-        </html>
-    }
-}
-
 #[derive(Clone)]
-struct ExpensivelyComputed;
+struct TheContext;
 
 #[component]
 pub fn Parent() -> impl IntoView {
-    provide_context(ExpensivelyComputed);
-
+    provide_context(TheContext);
     view! { <Outlet /> }
 }
 
 #[component]
 pub fn Child() -> impl IntoView {
-    use_context::<ExpensivelyComputed>().expect("ExpensivelyComputed context not provided.");
+    use_context::<TheContext>().expect("TheContext not provided.");
 }
 
 #[component]
@@ -39,14 +21,8 @@ pub fn App() -> impl IntoView {
     view! {
         <Router>
             <Routes fallback=|| ()>
-                <ParentRoute
-                    path=path!("/")
-                    view=move || Parent
-                >
-                    <Route
-                        path=path!("")
-                        view=Child
-                    />
+                <ParentRoute path=path!("/") view=|| Parent >
+                    <Route path=path!("") view=Child />
                 </ParentRoute>
             </Routes>
         </Router>
